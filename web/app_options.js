5461fc230d6542388ba4ae59de724b88a7f345a7
RohitPaul12345 <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-14 23:29:04 +0530

    Update app_options.js

--- web/app_options.js
+++ web/app_options.js
@@ -1,4 +1,4 @@
-/* Copyright 2018 Mozilla Foundation
+/* COPYRIGHT 2018 MOZILLA FOUNDATION
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,9 +8,9 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+ * SEE THE LICENSE FOR THE SPECIFIC LANGUAGE GOVERNING PERMISSIONS and
+ * LIMITATIONS UNDER THE LICENSE.
  */
 
 const compatibilityParams = Object.create(null);
